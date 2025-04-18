c99624c791947adc6ac696118a5d12f4323de985
RIDHAM KUMAR <ridhamverma843@gmail.com>
2025-04-18 13:54:18 +0530

    Initial comment: Added Database file

---
+++ DatabaseSetup/creatre-database.sql
@@ -0,0 +1,29 @@
+﻿-- Create the database
+CREATE DATABASE EmployeeCRUDapplication;
+GO
+
+-- Use the new database
+USE EmployeeCRUDapplication;
+GO
+
+-- Create the Employee table
+CREATE TABLE [dbo].[Employee](
+    [EmployeeID] INT IDENTITY(1,1) PRIMARY KEY,
+    [EmpName] VARCHAR(50),
+    [EmailID] NVARCHAR(50),
+    [Department] VARCHAR(20),
+    [Salary] INT
+);
+GO
+
+---
+
+
+
+--If you want to pre-fill some employee data for testing, add this to the bottom of your script:
+
+
+INSERT INTO Employee (EmpName, EmailID, Department, Salary)
+VALUES
+('John Doe', 'john@example.com', 'IT', 50000),
+('Jane Smith', 'jane@example.com', 'HR', 45000);
